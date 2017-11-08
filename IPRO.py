83cc337d85dd550e96b75ec47e7eff8762cdfa4f
BinBin He <hebinsquared@gmail.com>
2017-11-08 01:03:46 -0600

    Outputs full sorted file

--- IPRO.py
+++ IPRO.py
@@ -1,48 +1,41 @@
-
+# Python 3
 # coding: utf-8
 
-# In[3]:
 
 import pandas as pd
 import numpy as np
 
-
-# In[33]:
-
-#generate Synthatic Data
-df2 = pd.DataFrame(np.random.rand(100, 4)*5,columns=['width', 'length', 'depth', 'Traffic Density'])
-df2['Traffic Density'] = np.random.rand(100,4)*2000// 1
-#save generic data to csv file
-df2.to_csv('Synthatic_Data.csv',sep=',',header=True,index=False)
-
-
-# In[51]:
-
-#load data from csv file
-data = pd.read_csv('Synthatic_Data.csv',index_col=False)
-data.drop_duplicates(inplace=True)
-
-
-# In[53]:
-
 #this is mock algorithmm we probably gotta redefine it.
 def the_almighty_algorithm(row):
-    return 5*row['width']+0.3*row['length']+20*row['depth']+np.log(row['Traffic Density'])
+    return 10*row['width']+5*row['length']+20*row['depth']+np.log(row['Traffic Density'])
 
+def main():
+    #generate Synthatic Data
+    # np.random.rand(x,y) produces x rows and y columns of values from 0~1
+    df2 = pd.DataFrame(np.random.rand(100, 4)*5,columns=['width', 'length', 'depth', 'Traffic Density'])
 
-# In[55]:
+    df2['Traffic Density'] = np.random.rand(100,1)*2000// 1
 
-#apply algorithm on data
-data['score'] = data.apply(lambda row: the_almighty_algorithm(row), axis=1)
+    #save generic data to csv file
+    df2.to_csv('Synthatic_Data.csv',sep=',',header=True,index=False)
 
+    #load data from csv file
+    data = pd.read_csv('Synthatic_Data.csv',index_col=False)
 
-# In[69]:
+    #remove duplicates, may want to do something else with this
+    data.drop_duplicates(inplace=True)
 
-#rank the data based on score by algorithm
-print(data.sort_values(by ='score',axis=0,ascending=False).head())
+    #apply algorithm on dat
+    data['score'] = data.apply(lambda row: the_almighty_algorithm(row), axis=1)
 
+    #output filtered data to csv file
+    #data.to_csv('filtered_data.csv',sep=',',header=True,index=False)
+ 
+    #rank the data based on score by algorithm
+    sorted_data = data.sort_values(by ='score',axis=0,ascending=False)
 
-# In[ ]:
+    #output sorted data to csv file
+    sorted_data.to_csv('sorted_data.csv',sep=',',header=True,index=False)
+    return print('Success')
 
-
-
+main()
